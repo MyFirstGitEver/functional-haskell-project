8a4803c92b346f661da2cc779e7ef5701bc9b3a6
DUC <fordevok@gmail.com>
2023-08-13 20:18:14 +0700

    day 6

--- advent-of-code/day-5/main.hs
+++ advent-of-code/day-5/main.hs
@@ -1,16 +1,26 @@
 import Data.Tuple
 import System.IO
 
+checkCharFrom :: Char -> Int -> Int -> String -> Bool
+checkCharFrom ch index to str
+    | (index == to + 1) = True
+    | (ch == str!!index) = False
+    | otherwise = checkCharFrom ch (index + 1) to str
 
+isAllDistinct :: String -> Int -> Bool
+isAllDistinct str index
+    | (index == (length str)) = True
+    | (not (checkCharFrom (str!!index) (index + 1) (length str - 1) str)) = False
+    | otherwise = isAllDistinct str (index + 1)
 
 subString :: Int -> Int -> String -> String
 subString l r str = (take (r - l + 1) (drop l str))
 
-checkRecentDifferent :: String -> Int -> Int
-checkRecentDifferent str index
-    | (subStringDifferent (subString index (index + 3) str)) = (index + 3)
-    | otherwise = checkRecentDifferent str (index + 1)
+solve :: String -> Int -> Int
+solve str index
+    | (isAllDistinct (subString index (index + 13) str) 0) = (index + 13)
+    | otherwise = (solve str (index + 1))
 
 main = do
     x <- readFile "input.txt"
-    print(checkRecentDifferent x 0)
+    print(solve x 0)
